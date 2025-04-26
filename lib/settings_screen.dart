9b5937124cc00c763737cc1f24ac93a0d42c6075
mdrahib46 <nahid179846@bsdi-bd.org>
2025-04-26 23:21:00 +0600

    implement Change Notifire

--- lib/settings_screen.dart
+++ lib/settings_screen.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_for_professional/counter_controller.dart';
 
+final CounterController counterController = CounterController();
+
 class SettingsScreen extends StatefulWidget {
+  const SettingsScreen({super.key});
+
   @override
   State<SettingsScreen> createState() => _SettingsScreenState();
 }
@@ -9,21 +13,25 @@ class SettingsScreen extends StatefulWidget {
 class _SettingsScreenState extends State<SettingsScreen> {
   @override
   Widget build(BuildContext context) {
-    final CounterController? counterController = CounterController.of(context);
     return Scaffold(
       appBar: AppBar(title: Text('Setting Screen')),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          counterController?.increment();
+          counterController.increment();
         },
         child: Icon(Icons.add),
       ),
       body: Center(
         child: Column(
           children: [
-            Text(
-              '${counterController?.count}',
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
+            ListenableBuilder(
+              listenable: counterController,
+              builder: (context, child) {
+                return Text(
+                  '${counterController.count}',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
+                );
+              },
             ),
           ],
         ),
